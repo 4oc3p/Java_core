c4954bd2f6015d4b340c5ac8f89dec2bac64b91a
4oc3p <l4oc3pl@gmail.com>
2017-03-08 03:51:00 +0200

    Homework_arr_add_rem_2

--- src/main/java/homework/arrays_2/ArrAddRemove.java
+++ src/main/java/homework/arrays_2/ArrAddRemove.java
@@ -17,24 +17,37 @@ public class ArrAddRemove {
     }
 
     public void add(int num) {
-        int[] arr = new int[this.arr.length + 1];
+        int[] tempArr = new int[this.arr.length + 1];
         for (int i = 0; i < this.arr.length; i++) {
-            arr[i] = this.arr[i];
+            tempArr[i] = this.arr[i];
         }
-        arr[arr.length - 1] = num;
-        this.arr = arr;
+        tempArr[tempArr.length - 1] = num;
+        this.arr = tempArr;
     }
 
+    public void add2(int num) {
+        int[] tempArr = Arrays.copyOf(this.arr, this.arr.length + 1);
+        tempArr[tempArr.length - 1] = num;
+        this.arr = tempArr;
+    }
+
+
     public void remove(int index) {
-        int arr[] = new int[this.arr.length - 1];
-        for (int i = 0; i < arr.length; i++) {
+        int tempArr[] = new int[this.arr.length - 1];
+        for (int i = 0; i < tempArr.length; i++) {
             if (i >= index) {
-                arr[i] = this.arr[i + 1];
+                tempArr[i] = this.arr[i + 1];
             } else {
-                arr[i] = this.arr[i];
+                tempArr[i] = this.arr[i];
             }
         }
-        this.arr = arr;
+        this.arr = tempArr;
+    }
+
+    public void remove2(int index) {
+        int tempArr[] = Arrays.copyOf(this.arr, this.arr.length - 1);
+        System.arraycopy(this.arr, index + 1, tempArr, index, tempArr.length - index);
+        this.arr = tempArr;
     }
 
     @Override
@@ -45,10 +58,12 @@ public class ArrAddRemove {
     }
 
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 6};
-        ArrAddRemove test = new ArrAddRemove(arr);
-        test.remove(2);
-        test.add(12);
+        int[] arr1 = {1, 2, 3, 4, 5, 6};
+        ArrAddRemove test = new ArrAddRemove(arr1);
+//        test.remove(4);
+        test.remove2(4);
+//        test.add(12);
+        test.add2(12);
         System.out.println(test);
     }
 }
