f12259bb1e4da1cf6f57f3f73a2dbd1786146f16
4oc3p <l4oc3pl@gmail.com>
2017-06-13 17:35:05 +0300

    v3

--- src/main/java/codewars/Keypad.java
+++ src/main/java/codewars/Keypad.java
@@ -7,32 +7,14 @@ import java.util.List;
  * Created by 4oc3p on 13.06.2017. Java_core
  */
 public class Keypad {
-
-    private static final List<Character[]> list = Arrays.asList(
-            new Character[]{32, 48},  //0
-            new Character[]{49},  //1
-            new Character[]{97, 98, 99, 50},
-            new Character[]{100, 101, 102, 51},
-            new Character[]{103, 104, 105, 52},
-            new Character[]{106, 107, 108, 53},
-            new Character[]{109, 110, 111, 54},
-            new Character[]{112, 113, 114, 115, 55},
-            new Character[]{116, 117, 118, 56},
-            new Character[]{119, 120, 121, 122, 57},
-            new Character[]{35},
-            new Character[]{42});
+    private static String[] keys = {"1", "ABC2", "DEF3", "GHI4", "JKL5", "MNO6", "PQRS7", "TUV8", "WXYZ9", "*", " 0", "#"};
 
     public static int presses(String phrase) {
-        int sum = 0;
-        for (Character c : phrase.toLowerCase().toCharArray()) {
-            for (Character[] ch : list) {
-                for (int j = 0; j < ch.length; j++) {
-                    if (c.equals(ch[j])) {
-                        sum += j + 1;
-                    }
-                }
-            }
-        }
-        return sum;
+        int nPresses = 0;
+
+        for (char c : phrase.toUpperCase().toCharArray())
+            for (String s : keys)
+                nPresses += s.indexOf(c) + 1;
+        return nPresses;
     }
 }
